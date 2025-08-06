71176136af5299bb60aa3741c262e1f55dece7fb
skyfroger <skyfroger@gmail.com>
2025-08-06 14:09:17 +0300

    feat: первая версия расширения

--- _extensions/jsavblock/jsavblock.lua
+++ _extensions/jsavblock/jsavblock.lua
@@ -1,6 +1,44 @@
+local function writeEnvironments()
+  if quarto.doc.is_format("html:js") then
+    quarto.doc.add_html_dependency({
+      name = "jquery",
+      version = "2.2.4",
+      scripts = {
+        { path = "js/jquery.min.js", afterBody = "true" },
+        { path = "js/jquery-ui.min.js", afterBody = "true" },
+        { path = "js/jquery.transit.js", afterBody = "true" }
+      }
+    })
+    quarto.doc.add_html_dependency({
+      name = "JSAV",
+      version = "1.0.1",
+      scripts = {
+        { path = "js/JSAV-min.js",    afterBody = "true" },
+        { path = "js/dagre.min.js", afterBody = "true" },
+        { path = "js/raphael.js", afterBody = "true" },
+      },
+      stylesheets = { "css/JSAV.css", "css/styles.css" }
+    })
+  end
+end
 
--- Reformat all heading text 
-function Header(el)
-  el.content = pandoc.Emph(el.content)
-  return el
+
+function createJSAV(div)
+  writeEnvironments()
+  local divControls = pandoc.Div({}, {class = "jsavcontrols"})
+  local divOutput = pandoc.Div({}, {class = "jsavoutput jsavline"})
+  local spanCounter = pandoc.Span({}, {class = "jsavcounter"})
+  
+  table.insert(div.content, spanCounter)
+  table.insert(div.content, divControls)
+  table.insert(div.content, divOutput)
+  return div
+end
+
+if quarto.doc.isFormat("html:js") then
+  Div = function(div)
+    if div.classes:includes("jsavblock") then
+      return createJSAV(div)
+    end
+  end
 end
